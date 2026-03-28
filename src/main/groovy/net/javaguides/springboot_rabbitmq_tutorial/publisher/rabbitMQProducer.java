d182bf5d3e110cc2e75c3800c3a1df5571e57a6b
BogdanDolia <cr.co.erph@gmail.com>
2026-03-28 13:20:28 +0200

    Add rabbitMQProducer service for sending messages to RabbitMQ

---
+++ src/main/groovy/net/javaguides/springboot_rabbitmq_tutorial/publisher/rabbitMQProducer.java
@@ -0,0 +1,29 @@
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class rabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(rabbitMQProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public rabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent -> %s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
